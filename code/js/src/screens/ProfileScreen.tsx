bb221267d7958a4c8417c0868c29022df3692f1a
A48264 - João Pereira <a48264@alunos.isel.pt>
2023-12-07 12:18:09 +0000

    ProfileScreen update.
    Now it shows the username, total score and games played.

--- code/js/src/screens/ProfileScreen.tsx
+++ code/js/src/screens/ProfileScreen.tsx
@@ -1,17 +1,64 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../services/Auth";
 import { useFetch } from "../custom-hooks/useFetch";
 
 
 export const ProfileScreen = () => {
+    const currentUser = useContext(AuthContext);
+    console.log(currentUser.user);
+
+    if (!currentUser || !currentUser.user) {
+        // Display a message guiding the user to log in again
+        return (
+            <div>
+                <Link to="/home">Return</Link>
+                <p>Your session has expired or you are not logged in.</p>
+                <p>Please <Link to="/login">login</Link> to view this page.</p>
+            </div>
+        );
+    }
+
+    /*
+    GET http://localhost:8080/rankings/JP
+    {
+        "class": [
+            "UserRanking"
+        "properties": {
+            "userRanking": {
+                "user": "JP",
+                "score": 450,
+                "ngames": 5
+            }
+        },
+        "links": [],
+        "entities": [],
+        "actions": []
+    }
+    */
+
+    const userRankingUri = "api/rankings/" + currentUser.user.username;
+    const { data: content, loading, error } = useFetch<{
+        properties: { userRanking: { user: string; score: number; ngames: number } }
+    }>({ uri: userRankingUri });
+
+    if (loading)
+        return <div>Loading...</div>;
+
+    if (error || !content || !content.properties || !content.properties.userRanking)
+        return <div>Error fetching data...</div>;
+
     return (
         <div>
             <Link to="/home">Return</Link>
             <h1>Profile</h1>
-            <h2>Username</h2>
-            <text>Admin</text>
-            <h2>Rank</h2>
-            <text>Global Elite</text>
+            <h2>Username:</h2>
+            <p style={{ paddingLeft: '20px' }}>{currentUser.user.username}</p>
+            <h2>Rank:</h2>
+            <p style={{ paddingLeft: '20px' }}>
+                Total score: {content.properties.userRanking.score} <br />
+                Games played: {content.properties.userRanking.ngames}
+            </p>
         </div>
     );
 }
