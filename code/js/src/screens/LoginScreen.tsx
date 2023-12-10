beb85699fa0950ce77f493df6cef2fedf63ccb5a
Afonso Cabaço <afonso.cabaco@icloud.com>
2023-12-10 21:51:25 +0000

    fix

--- code/js/src/screens/LoginScreen.tsx
+++ code/js/src/screens/LoginScreen.tsx
@@ -1,9 +1,21 @@
-import React, { useState, ChangeEvent } from "react";
+import React, { useState, ChangeEvent, useContext } from "react";
+import { makeStyles } from "@material-ui/core";
 import { useLocation, Navigate } from "react-router-dom";
 import { _fetch } from "../custom-hooks/useFetch";
 import { useSetUser } from "../services/Auth";
 import { useCookies } from "react-cookie";
 
+
+const useStyles = makeStyles(
+    (theme) => ({
+        formControl: {
+            marginRight: theme.spacing(1)
+        },
+        labelSpacing: {
+            marginLeft: theme.spacing(1)
+        }
+    })
+);
 
 async function login(
     username: string, password: string
@@ -15,13 +27,12 @@ async function login(
 
 function CallLoginScreen() {
     const setUser = useSetUser();
-    setUser(null);
-    const [inputs, setInputs] =
-        useState({ username: '', password: '' });
+
+    const [inputs, setInputs] = useState({ username: '', password: '' });
+    const [redirect, setRedirect] = useState(false);
+    const [submitting, setSubmitting] = useState(false);
     const [error, setError] = useState(undefined)
     const [cookies, setCookie] = useCookies(["Token"]);
-    const [submitting, setSubmitting] = useState(false);
-    const [redirect, setRedirect] = useState(false);
     const location = useLocation();
 
     if (redirect)
@@ -37,21 +48,20 @@ function CallLoginScreen() {
     async function acceptSubmit(ev: React.FormEvent<HTMLFormElement>) {
         ev.preventDefault();
         setSubmitting(true);
-
         try {
             const resp = await login(inputs.username, inputs.password);
-            if (
-                resp && resp.properties.username &&
-                resp.properties.id && resp.properties.token
-            ) {
+            console.log(resp);
+            if (resp)   {
+                console.log(resp.properties);
                 const user = {
                     username: resp.properties.username,
                     id: resp.properties.id,
                     token: resp.properties.token
                 };
-                setCookie(
-                    "Token", resp.properties.token,
-                    { path: '/' });
+                setCookie("Token", resp.properties.token, 
+                {
+                    path: '/'
+                });
                 setUser(user);
                 setRedirect(true);
             } else setError(<p>Login failed. Please check your credentials.</p>);
@@ -63,46 +73,50 @@ function CallLoginScreen() {
     }
 
     return <>
-        {!submitting ?
-            <form
-                onSubmit={acceptSubmit}
-                style={{
-                    display: 'flex', flexDirection: 'column',
-                    alignItems: 'center', padding: 40
-                }}
-            >
-                <fieldset disabled={submitting}>
-                    <h1 style={{ textAlign: 'center' }}>Login</h1>
-                    <div>
-                        <label htmlFor="username">Username</label>
-                        <input
-                            id="username" type="text" name="username"
-                            value={inputs.username} onChange={acceptChange}
-                        />
-                    </div>
-                    <div>
-                        <label htmlFor="password">Password</label>
-                        <input
-                            id="password" type="password" name="password"
-                            value={inputs.password} onChange={acceptChange}
-                        />
-                    </div>
-                    <div>
-                        <button
-                            type="submit"
-                            disabled={!inputs.username || !inputs.password}
-                        > Login
-                        </button> <br />
-                        <a>Don't have an account? </a>
-                        <a href="/register">Sign up</a>
-                    </div>
-                </fieldset>
-                {error}
-            </form> : null
-        }
+            {!submitting ?
+                <form
+                    onSubmit={acceptSubmit}
+                    style={{
+                        display: 'flex', flexDirection: 'column', alignItems: 'center', padding: 40
+                    }}
+                >
+                    <fieldset disabled={submitting}>
+                        <h1 style={{ textAlign: 'center' }}>Login</h1>
+                        <div>
+                            <label htmlFor="username">Username</label>
+                            <input
+                                id="username" type="text" name="username"
+                                value={inputs.username} onChange={acceptChange}
+                            />
+                        </div>
+                        <div>
+                            <label htmlFor="password">Password</label>
+                            <input
+                                id="password" type="password" name="password"
+                                value={inputs.password} onChange={acceptChange}
+                            />
+                        </div>
+                        <div>
+                            <button
+                                type="submit"
+                                disabled={!inputs.username || !inputs.password}
+                            > Login
+                            </button> <br />
+                            <a>Don't have an account? </a>
+                            <a href="/register">Sign up</a>
+                        </div>
+                    </fieldset>
+                    {error}
+                </form> :
+                null
+            }
     </>
 }
 
 export const LoginScreen = () => {
-    return (<div><CallLoginScreen /></div>);
+    return (
+        <div>
+            <CallLoginScreen />
+        </div>
+    );
 }
--- code/js/src/screens/utils.tsx
+++ code/js/src/screens/utils.tsx
@@ -62,71 +62,12 @@ function NavBar() {
                         </Button>
                     </li>
                     <li style={{ float: 'right' }}>
-                        <Button onClick={handleLogout} className="active" style={{
-                            display: 'block', color: 'white', textAlign: 'center',
-                            padding: '14px 16px', textDecoration: 'none'
-                        }}> Logout
-                        </Button>
                         <Link to="/profile" className='active' style={{
                             display: 'block', color: 'white', textAlign: 'center',
                             padding: '14px 16px', textDecoration: 'none'
                         }}> My Profile
                         </Link>
                     </li>
-                </>
-                    :
-                    <>
-            <li style={{ float: 'left' }}>
-                <Link to="/" className="active"
-                    style={{
-                        display: 'block', color: 'white', textAlign: 'center',
-                        padding: '14px 16px', textDecoration: 'none'
-                    }}> Home
-                </Link>
-            </li>
-            <li style={{ float: 'left' }}>
-                <Link to="/rankings" className="active"
-                    style={{
-                        display: 'block', color: 'white', textAlign: 'center',
-                        padding: '14px 16px', textDecoration: 'none'
-                    }}> Rankings
-                </Link>
-            </li>
-            <li style={{ float: 'left' }}>
-                <Link to="/authors" className="active"
-                    style={{
-                        display: 'block', color: 'white', textAlign: 'center',
-                        padding: '14px 16px', textDecoration: 'none'
-                    }}> Authors
-                </Link>
-            </li>
-            <li style={{ float: 'left' }}>
-                        <Link to="/about" className="active"
-                            style={{
-                                display: 'block', color: 'white', textAlign: 'center',
-                                padding: '14px 16px', textDecoration: 'none'
-                            }}> About
-                        </Link>
-                    </li>
-            <li style={{ float: 'right' }}>
-                <Link to="/login" className="active"
-                    style={{
-                        display: 'block', color: 'white', textAlign: 'center',
-                        padding: '14px 16px', textDecoration: 'none'
-                    }}> Login
-                </Link>
-            </li>
-            <li style={{ float: 'right' }}>
-                <Link to="/register" className="active"
-                    style={{
-                        display: 'block', color: 'white', textAlign: 'center',
-                        padding: '14px 16px', textDecoration: 'none'
-                    }}> Sign up
-                </Link>
-            </li>
-        </>}
-                </ul>
-            </div>
                 </> : <>
                     <li style={{ float: 'left' }}>
                         <Link to="/" className="active" style={{
